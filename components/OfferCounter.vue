e2368d1cf5c1d8562233f93a54da806fc569a0a3
Uzyxen <michal912456@gmail.com>
2024-04-20 23:28:28 +0200

    offer counter counting from 0 to target

--- components/OfferCounter.vue
+++ components/OfferCounter.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>
-
-    </div>
+    <h1>{{ counter.toLocaleString() }}</h1>
 </template>
 
 <script setup>
+    const counter = ref(0);
+    const target = ref(92673);
 
+    onMounted(() => {
+        setInterval(() => {
+            if(counter.value < target.value){
+                counter.value += 952;
+
+                if(counter.value > target.value)
+                    counter.value = target.value;
+            }
+        }, 1);
+    });
 </script>
 
 <style scoped>
-
+    h1{
+        font-weight: 900;
+        font-size: 60px;
+        color: #6244DB;
+    }
 </style>
