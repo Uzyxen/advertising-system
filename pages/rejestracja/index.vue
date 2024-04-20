39e4e8ad1fd029fb5aab26532d2ba21f74086bfc
Uzyxen <michal912456@gmail.com>
2024-04-21 00:40:47 +0200

    register page check if data not empty

--- pages/rejestracja/index.vue
+++ pages/rejestracja/index.vue
@@ -1,10 +1,11 @@
 <template>
     <div id="root">
         <div id="blur-circle"></div>
-        <div id="login-box">
-            <h2>Zaloguj się</h2>
 
-            <form @submit.prevent="Login" method="post"> 
+        <div id="register-box">
+            <h2>Zarejestruj się</h2>
+
+            <form @submit.prevent="Register" method="post">
                 <div>
                     <label for="email">Email:</label>
                     <input :class="{ 'input-error': error.emailErr }" placeholder="Adres e-mail" type="text" name="email" v-model="data.email">
@@ -13,21 +14,18 @@
 
                 <div>
                     <label for="password">Hasło:</label>
-                    <input :class="{ 'input-error': error.passwordErr }" placeholder="Hasło" type="password" name="password" v-model="data.password">
+                    <input :class="{ 'input-error': error.passwordErr }" placeholder="Hasło" type="text" name="password" v-model="data.password">
                     <span class="error">{{ error.passwordErr }}</span>
                 </div>
 
-                <button type="submit">Zaloguj się</button>
+                <div>
+                    <label for="password_2">Powtórz hasło:</label>
+                    <input :class="{ 'input-error': error.password_2Err }" placeholder="Powtórz hasło" type="text" name="password_2" v-model="data.password_2">
+                    <span class="error">{{ error.password_2Err }}</span>
+                </div>
+
+                <button type="submit">Zarejestruj się</button>
             </form>
-
-            <div id="register">
-                <p>Nie masz konta?</p>
-                <Nuxt-link to="/rejestracja">Zarejestruj się</Nuxt-link>
-            </div>
-
-            <p id="for-companies">
-                <Nuxt-link to="/firma/logowanie">Logowanie dla firm</Nuxt-link>
-            </p>
         </div>
     </div>
 </template>
@@ -39,15 +37,17 @@
 
     const data = ref({
         email: '',
-        password: ''
+        password: '',
+        password_2: ''
     });
 
     const error = ref({
-       emailErr: '',
-       passwordErr: '' 
+        emailErr: '',
+        passwordErr: '',
+        password_2Err: ''
     });
 
-    function Login() {
+    function Register() {
         if(data.value.email == '') {
             error.value.emailErr = 'Wypełnij pole!';
         } else {
@@ -59,9 +59,14 @@
         } else {
             error.value.passwordErr = '';
         }
+
+        if(data.value.password_2 == '') {
+            error.value.password_2Err = 'Wypełnij pole!';
+        } else {
+            error.value.password_2Err = '';
+        }
     }
 </script>
-
 
 <style scoped>
     .error{
@@ -74,13 +79,31 @@
         border-color: #FA4132;
     }
 
-    input {
-        border: 1px solid var(--asc-bg-border)
+   #root {
+        height: 100vh;
     }
 
-    #root{
-        width: 100%;
-        height: 100vh;
+    input {
+        border: 1px solid var(--asc-bg-border);
+    }
+
+    #register-box{
+        padding: 50px 100px 0;
+        width: 400px;
+        height: 590px;
+        position: absolute;
+        top: 30%;
+        left: 50%;
+        transform: translate(-50%, -30%);
+        box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.15);
+        border-radius: 2px;
+        background-color: #FFF;
+    }
+
+    #register-box form div {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
     }
 
     #blur-circle{
@@ -96,63 +119,33 @@
         z-index: -9999;
     }
 
-    #login-box{
-        background-color: #FFF;
-        padding: 50px 100px 0;
-        width: 400px;
-        min-height: 550px;
-        position: absolute;
-        top: 30%;
-        left: 50%;
-        transform: translate(-50%, -30%);
-        box-shadow: 0px 1px 5px 0px rgba(0, 0, 0, 0.15);
-        border-radius: 2px;
-        z-index: 99;
-        display: flex;
-        flex-direction: column;
-    }
-
-    #login-box h2{
+    #register-box h2{
         font-size: 26px;
         text-align: center;
     }
 
-    #login-box p{
+    #register-box p{
         text-align: center;
         margin: 10px 0 50px;
     }
 
-    #register{
-        flex: 1;
-        display: flex;
-        gap: 10px;
-    }
-
-    #register p, #register a {
-        margin: 20px 0 0 !important;
-    }
-
-    #register a {
-        color: rgb(16, 125, 214);
-    }
-
-    #login-box form div{
+    #register-box {
         display: flex;
         flex-direction: column;
         gap: 10px;
     }
 
-    #login-box form div label{
+    #register-box label{
         font-size: 20px;
     }
 
-    #login-box form div input{
+    #register-box input{
         padding: 15px;
         height: 50px;
         box-sizing: border-box;
     }
 
-    #login-box button{
+    #register-box button[type='submit']{
         margin-top: 10px;
         width: 400px;
         background-color: #6244DB;
@@ -160,15 +153,5 @@
         padding: 15px;
         font-size: 18px;
         cursor: pointer;
-    }
-
-    #login-box p#for-companies{
-        text-align: center;
-        margin: 40px 0 20px 0;
-    }
-    
-    #login-box p#for-companies a{
-        text-decoration: none;
-        color: #6244DB;
     }
 </style>
