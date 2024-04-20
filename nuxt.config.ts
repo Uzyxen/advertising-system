19b883e45fc5f6d064a62d73c72e1980c100d5ff
Uzyxen <michal912456@gmail.com>
2024-04-20 17:45:56 +0200

    create home page section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt"]
+  modules: ["@pinia/nuxt"],
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
 })
