7a9aa7152c8579bdddf8c1f5c32a0a3359370021
Uzyxen <michal912456@gmail.com>
2024-04-20 17:53:43 +0200

    section title as a component

---
+++ components/SectionTitle.vue
@@ -0,0 +1,28 @@
+<template>
+    <h2>
+        <slot />
+    </h2>
+</template>
+
+<style scoped>
+    h2{
+        text-align: center;
+        color: #231d2b;
+        font-size: 42px;
+        font-weight: 800;
+        position: relative;
+    }
+
+    h2::before{
+        content: '';
+        position: absolute;
+        background-color: var(--asc-bg);
+        left: 0;
+        bottom: 0;
+        height: 50%;
+        width: 100%;
+        opacity: 0.2;
+        z-index: -1;
+    }
+
+</style>
