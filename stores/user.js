b9eb7af2cb44fab7f7d45b5f3379b4e042ea9e6d
Uzyxen <michal912456@gmail.com>
2024-04-21 23:34:08 +0200

    user store login/get-data/logout methods

--- stores/user.js
+++ stores/user.js
@@ -10,6 +10,63 @@ export const useUserStore = defineStore('userStore', {
         gender: '',
         age: 0,
         country: '',
-        message: ''
     }),
+
+    actions: {
+        async Login(user_data) {
+            const headers = useRequestHeaders(['cookie']);
+            const { data: response } = await useFetch('http://localhost/advertising-system/api/user/LogIn.php', { headers, credentials: 'include', method: 'POST', body: user_data, responseType: 'json' });
+         
+            if(response.value.data) {
+                this.logged = response.value.data.logged;
+                this.first_name = response.value.data.first_name;
+                this.last_name = response.value.data.last_name;
+                this.description = response.value.data.description;
+                this.phone_number = response.value.data.phone_number;
+                this.email = response.value.data.email;
+                this.position = response.value.data.position;
+                this.gender = response.value.data.gender;
+                this.age = response.value.data.age;
+                this.country = response.value.data.country;
+            } else if(response.value) {
+                console.log(response.value);
+            }
+        },
+
+        async GetUserData() {
+            const headers = useRequestHeaders(['cookie']);
+            const { data: response } = await useFetch('http://localhost/advertising-system/api/user/GetUserData.php', { headers, credentials: 'include', responseType: 'json' });
+
+            if(response.value.data) {
+                this.logged = response.value.data.logged;
+                this.first_name = response.value.first_name;
+                this.last_name = response.value.last_name;
+                this.description = response.value.description;
+                this.phone_number = response.value.phone_number;
+                this.email = response.value.email;
+                this.position = response.value.position;
+                this.gender = response.value.gender;
+                this.age = response.value.age;
+                this.country = response.value.country;
+            } else if(response.value) {
+                console.log(response.value);
+            }
+        },
+
+        async LogOut() {
+            const headers = useRequestHeaders(['cookie']);
+            await useFetch('http://localhost/advertising-system/api/user/LogOut.php', { headers, credentials: 'include' });
+
+            this.logged = false;
+            this.first_name = '';
+            this.last_name = '';
+            this.description = '';
+            this.phone_number = '';
+            this.email = '';
+            this.position = '';
+            this.gender = '';
+            this.age = '';
+            this.country = '';
+        }
+    }
 });
