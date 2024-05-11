c3aa138362c019b50c88b9ad6c6b3780f27f5728
Uzyxen <michal912456@gmail.com>
2024-05-11 13:55:54 +0200

    create purple button component with slot

---
+++ components/buttons/PurpleButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+
+<style scoped>
+    button {
+        color: var(--asc-txt-alt);
+        background-color: var(--asc-bg);
+        font-size: 18px;
+        padding: 10px 25px;
+        width: 200px;
+        cursor: pointer;
+        border-radius: 2px;
+    }
+</style>
