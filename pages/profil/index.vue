d55bcf5e33a47ef1fd302aefe8a5f41e5a462a85
Uzyxen <michal912456@gmail.com>
2024-05-11 12:56:26 +0200

    change layout

--- pages/profil/index.vue
+++ pages/profil/index.vue
@@ -17,54 +17,60 @@
             </nav>
 
             <main>
-                <section id="left-side">
-                    <div id="user-main">
-                        <div id="image">
-                            <div id="img" @click="isModalVisible = true; modalType = 0">
-                                <div id="upload-image">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
-                                        <path d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192c0 35.3 28.7 64 64 64s64-28.7 64-64H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/>
-                                    </svg>
+                <div id="main-flex">
+                    <section id="user-top">
+                        <div id="user-main">
+                            <div id="image">
+                                <div id="img" @click="isModalVisible = true; modalType = 0">
+                                    <div id="upload-image">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc. -->
+                                            <path d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192c0 35.3 28.7 64 64 64s64-28.7 64-64H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                            <h2 id="user-name">{{ userData.imie }} {{ userData.nazwisko }}</h2>
+                            <h3 id="user-position">{{ userData.stanowisko }}</h3>
+                        </div>
+
+                        <div id="user-alt">
+                            <h4></h4>
+                        </div>
+                    </section>
+
+                    <section id="main-section">
+                        <div>
+                            <EditHeader @edit="isModalVisible = true; modalType = 1">Podsumowanie zawodowe</EditHeader>
+                            <hr>
+                            <p>{{ userData.opis }}</p>
+                        </div>
+
+                        <div>
+                            <EditHeader @edit="isModalVisible = true; modalType = 2">Umiejętności</EditHeader>
+                            <hr>
+
+                            <div id="skills">
+                                <UserSkill v-for="skill in userSkills" :skill="skill.umiejetnosc" />
+                            </div>
+                        </div>
+
+                        <div>
+                            <EditHeader @edit="isModalVisible = true; modalType = 3">Doświadczenie</EditHeader>
+                            <hr>
+
+                            <div id="experience">
+                                <div id="experience-block" v-for="experience in userExperience">
+                                    <div id="abstract">
+                                        <div id="circle"></div>
+                                        <div id="line"></div>
+                                    </div>
+
+                                    <UserExperience :experience="experience"/>
                                 </div>
                             </div>
                         </div>
-                        <h2 id="user-name">{{ userData.imie }} {{ userData.nazwisko }}</h2>
-                        <h3 id="user-position">{{ userData.stanowisko }}</h3>
-                    </div>
-                </section>
-
-                <section id="main-section">
-                    <div>
-                        <EditHeader @edit="isModalVisible = true; modalType = 1">Podsumowanie zawodowe</EditHeader>
-                        <hr>
-                        <p>{{ userData.opis }}</p>
-                    </div>
-
-                    <div>
-                        <EditHeader @edit="isModalVisible = true; modalType = 2">Umiejętności</EditHeader>
-                        <hr>
-
-                        <div id="skills">
-                            <UserSkill v-for="skill in userSkills" :skill="skill.umiejetnosc" />
-                        </div>
-                    </div>
-
-                    <div>
-                        <EditHeader @edit="isModalVisible = true; modalType = 3">Doświadczenie</EditHeader>
-                        <hr>
-
-                        <div id="experience">
-                            <div id="experience-block" v-for="experience in userExperience">
-                                <div id="abstract">
-                                    <div id="circle"></div>
-                                    <div id="line"></div>
-                                </div>
-
-                                <UserExperience :experience="experience"/>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                    </section>
+                </div>
 
                 <section id="right-side">
 
@@ -153,13 +159,21 @@
 
     main {
         display: flex;
+        gap: 22px;
     }
 
-    main #left-side {
-        background-color: #FFF;
-        width: 300px;
+    #main-flex {
         display: flex;
         flex-direction: column;
+        width: calc(100% - 300px);
+        gap: 22px;
+    }
+
+    main #user-top {
+        background-color: #FFF;
+        width: 100%;
+        height: 300px;
+        display: flex;
     }
 
     #left-side #user-main {
@@ -229,10 +243,8 @@
     }
 
     section#main-section {
-        width: calc(100% - 650px);
+        width: 100%;
         background-color: #FFF;
-        border-left: 1px solid #DDD;
-        border-right: 1px solid #DDD;
     }
 
     #main-section > div hr {
