9ec53250df5ca4b64f66863c1efd6a44beafdfeb
Uzyxen <michal912456@gmail.com>
2024-05-12 23:15:58 +0200

    fetch also company name

--- backend/class/Offer.php
+++ backend/class/Offer.php
@@ -18,7 +18,10 @@
         }
 
         protected function getOffers() {
-            $sql = 'SELECT * from ogloszenia_firm';
+            $l_stmt = $this->connect()->prepare('SET lc_time_names = "pl_PL"');
+            $l_stmt->execute();
+
+            $sql = 'SELECT zdjecie_url, ogloszenie_id, tytul, opis, umowa, lokalizacja, wynagrodzenie_min, wynagrodzenie_max, czestotliwosc_wynagrodzenia, DATE_FORMAT(data_dodania, "%e %M %Y") AS data_dodania, data_dodania AS data_sort, (SELECT nazwa_firmy FROM firmy WHERE firma_id = ogloszenia_firm.firma_id) AS firma FROM ogloszenia_firm';
             $stmt = $this->connect()->prepare($sql);
 
             $stmt->execute();
--- components/Offer.vue
+++ components/Offer.vue
@@ -11,7 +11,7 @@
                 <h3>{{ offer.tytul }}</h3>
                 <p class="company">
                     <svg xmlns="http://www.w3.org/2000/svg" height="15" width="11" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. --><path d="M64 48c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16h80V400c0-26.5 21.5-48 48-48s48 21.5 48 48v64h80c8.8 0 16-7.2 16-16V64c0-8.8-7.2-16-16-16H64zM0 64C0 28.7 28.7 0 64 0H320c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64zm88 40c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H104c-8.8 0-16-7.2-16-16V104zM232 88h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H232c-8.8 0-16-7.2-16-16V104c0-8.8 7.2-16 16-16zM88 232c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H104c-8.8 0-16-7.2-16-16V232zm144-16h48c8.8 0 16 7.2 16 16v48c0 8.8-7.2 16-16 16H232c-8.8 0-16-7.2-16-16V232c0-8.8 7.2-16 16-16z"/></svg>
-                    {{ offer.company }}
+                    {{ offer.firma }}
                 </p>
                 <p class="location">
                     <svg xmlns="http://www.w3.org/2000/svg" height="15" width="11" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc. --><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
@@ -25,7 +25,7 @@
 
         <div class="offer-bottom">
             <p>
-                Opublikowano: {{ offer.data_dodani }}
+                Opublikowano: {{ offer.data_dodania }}
             </p>
         </div>
     </div>
