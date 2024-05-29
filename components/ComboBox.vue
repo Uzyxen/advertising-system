255e6302cdb5c8bba052438a45d52ef263983b6d
Uzyxen <michal912456@gmail.com>
2024-05-29 18:02:16 +0200

    feat: create reusable ComboBox component

---
+++ components/ComboBox.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="selected" @click="isCollapsed = !isCollapsed">
+        <p v-if="selected">{{ selected }}</p>
+        <p v-else>wybierz</p>
+
+        <div id="list" v-if="isCollapsed">
+            <div class="item" v-for="item in source" @click="selectItem(item)">
+                {{ item }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['source']);
+
+    const selected = ref('');
+    const isCollapsed = ref(false);
+
+    function selectItem(item) {
+        selected.value = item;
+    }
+</script>
+
+<style scoped>
+    #selected {
+        position: relative;
+        min-width: 100px;
+        height: 15px;
+        padding: 10px;
+        border: 1px solid #DDD;
+        cursor: pointer;
+    }
+
+    #list {
+        width: 100%;
+        background-color: var(--asc-bg-alt);
+        position: absolute;
+        left: -1px;
+        top: 36px;
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+        border: 1px solid #DDD;
+        border-top: none;
+    }
+
+    .item {
+        padding: 10px;
+        cursor: pointer;
+        width: calc(100% - 20px);
+    }
+
+    .item:hover {
+        background-color: #eee;
+    }
+</style>
--- components/modals/type/AddOffer.vue
+++ components/modals/type/AddOffer.vue
@@ -2,52 +2,99 @@
     <ModalHeader>Dodaj ofertę</ModalHeader>
 
     <div id="content">
-        <div>
+        <section>
             <h3>Tytuł: </h3>
             <input type="text" placeholder="tytuł">
-        </div>
+        </section>
         
-        <div>
+        <section>
             <h3>Opis: </h3>
             <textarea></textarea>
-        </div>
+        </section>
 
-        <div>
+        <section>
             <h3>Wynagrodzenie: </h3>
-            <div>
+            <div id="salary">
                 <input type="text">
-                - 
+                <span>-</span>
                 <input type="text"> zł
 
-                / 
-                <select>
-                    <option>miesiąc</option>
-                    <option>godzine</option>
-                </select>
-            </div>
-        </div>
+                <span>/</span>
 
-        <div>
+                <ComboBox :source="['miesiąc', 'godzinę']"/>
+            </div>
+        </section>
+
+        <section>
             <h3>Kontrakt</h3>
 
-            <select>
-                <option>Kontrakt B2B</option>
-                <option>Umowa o prace</option>
-            </select>
-        </div>
+            <ComboBox :source="['Kontrakt B2B', 'Umowa o pracę']" />
+        </section>
+
+        <section>
+            <h3>Zakres obowiązków</h3>
+
+            <div v-for="i in duties">
+                {{ i }}<input type="text">
+            </div>
+
+            <button @click="duties++">Dodaj nowy</button>
+        </section>
+
+        <section>
+            <h3>Wymagania</h3>
+
+            <div v-for="i in requirements">
+                {{ i }}<input type="text">
+            </div>
+
+            <button @click="requirements++">Dodaj nowy</button>
+        </section>
     </div>
 </template>
 
 <script setup>
-
+    const duties = ref(0);
+    const requirements = ref(0);
 </script>
 
 <style scoped>
     #content {
         width: 100%;
+        max-height: 500px;
+        overflow-y: auto;
+        padding: 0 10px;
+        display: flex;
+        flex-direction: column;
+        gap: 20px;
     }
 
     input {
         border: 1px solid #DDD;
+        width: calc(100% - 20px);
+        padding: 10px;
+    }
+
+    textarea {
+        border: 1px solid #DDD;
+        outline: none;
+        resize: none;
+        height: 200px;
+    }
+
+    h3 {
+        font-size: 20px;
+    }
+
+    section {
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+    }
+
+    #salary {
+        display: flex;
+        align-items: center;
+        gap: 20px;
     }
 </style>    
