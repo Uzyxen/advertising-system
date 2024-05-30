a8712a9683ab680d0f20cdf711afb0fc1ffaf631
Uzyxen <michal912456@gmail.com>
2024-05-30 16:02:23 +0200

    feat: add transition group

--- components/OffersList.vue
+++ components/OffersList.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="offer-list" v-if="offers">
-        <Offer :edit-mode="editMode" @offer-clicked="$emit('offer-clicked', offer.ogloszenie_id)" v-for="offer in offers" :key="offer.ogloszenie_id" :offer="offer" />
+        <TransitionGroup name="list">
+            <Offer :edit-mode="editMode" @offer-clicked="$emit('offer-clicked', offer.ogloszenie_id)" v-for="offer in offers" :key="offer.ogloszenie_id" :offer="offer" />
+        </TransitionGroup>
     </div>
 
     <div v-else>
@@ -18,5 +20,15 @@
         display: flex;
         flex-direction: column;
         row-gap: 15px;
+    }
+
+    .list-enter-active,
+    .list-leave-active {
+        transition: all 0.5s ease;
+    }
+    .list-enter-from,
+    .list-leave-to {
+        opacity: 0;
+        transform: translateX(30px);
     }
 </style>
